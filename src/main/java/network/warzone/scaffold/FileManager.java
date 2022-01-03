67676cebc8096cb0369c514462cb9faeb2b41d35
Mew2K <72997300+Mew2K@users.noreply.github.com>
2022-01-03 17:20:52 -0600

    Update FileManager.java

--- src/main/java/network/warzone/scaffold/FileManager.java
+++ src/main/java/network/warzone/scaffold/FileManager.java
@@ -1,8 +1,25 @@
 package network.warzone.scaffold;
 
+import com.google.common.base.Preconditions;
+import lombok.Data;
+import lombok.ToString;
+import network.warzone.scaffold.utils.config.Config;
+import network.warzone.scaffold.utils.config.ConfigFile;
+import org.apache.commons.io.FileUtils;
+
 import java.util.ArrayList;
 import java.io.File;
 
 public class FileManager {
+  
+  public static FileManager()
+  {
+    
+  }
+  
+  //Get all world file names
+  public static ArrayList<String> getFileNames() {
+      
+  }
   
 }
